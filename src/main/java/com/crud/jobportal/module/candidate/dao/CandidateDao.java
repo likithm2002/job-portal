264377dd0ff507844f4c248f2598a290082706de
likithmuniyappa <likithm2002@gmail.com>
2024-06-22 19:46:58 +0530

    update dao for admin insertion

--- src/main/java/com/crud/jobportal/module/candidate/dao/CandidateDao.java
+++ src/main/java/com/crud/jobportal/module/candidate/dao/CandidateDao.java
@@ -4,7 +4,7 @@ import com.crud.jobportal.module.candidate.dto.CandidateDto;
 import org.apache.coyote.BadRequestException;
 
 public interface CandidateDao {
-    CandidateDto createCandidate(CandidateDto candidateDto);
+    CandidateDto createCandidate(CandidateDto candidateDto) throws BadRequestException;
 
     CandidateDto getAdminById(Long id) throws BadRequestException;
 
