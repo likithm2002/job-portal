19b91fbd0b4336f78001586362ffd5133a9dd235
likithmuniyappa <likithm2002@gmail.com>
2024-06-24 00:40:45 +0530

    added association between job_applications and jobs

--- src/main/java/com/crud/jobportal/module/job/entity/Job.java
+++ src/main/java/com/crud/jobportal/module/job/entity/Job.java
@@ -1,12 +1,15 @@
 package com.crud.jobportal.module.job.entity;
 
+import com.crud.jobportal.module.jobapplication.entity.JobApplication;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 @Entity
 @Data
@@ -17,10 +20,13 @@ import java.util.Date;
 public class Job {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-
     private Long id;
+
     private String name;
     private String industry;
     private Long salary;
     private Date createdAt;
+
+    @OneToMany(mappedBy = "job", cascade = CascadeType.ALL)
+    private List<JobApplication> jobApplicationList = new ArrayList<>();
 }
