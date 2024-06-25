22daf3f76c4bff65143fdc063857c920d151a397
likithmuniyappa <likithm2002@gmail.com>
2024-06-25 20:59:15 +0530

    candidate shortlist dao and service implementation

--- src/main/java/com/crud/jobportal/module/candidate/service/impl/CandidateServiceImpl.java
+++ src/main/java/com/crud/jobportal/module/candidate/service/impl/CandidateServiceImpl.java
@@ -1,11 +1,18 @@
 package com.crud.jobportal.module.candidate.service.impl;
 
 import com.crud.jobportal.module.candidate.dao.CandidateDao;
+import com.crud.jobportal.module.candidate.dao.CandidateSkillDao;
 import com.crud.jobportal.module.candidate.dto.CandidateDto;
+import com.crud.jobportal.module.candidate.dto.CandidateSkillDto;
 import com.crud.jobportal.module.candidate.service.CandidateService;
 import com.crud.jobportal.module.candidate.vo.request.CreateCandidateRequest;
+import com.crud.jobportal.module.candidate.vo.request.CreateCandidateSkillRequest;
 import com.crud.jobportal.module.candidate.vo.request.UpdateCandidateRequest;
 import com.crud.jobportal.module.candidate.vo.response.CandidateResponse;
+import com.crud.jobportal.module.candidate.vo.response.CandidateSkillResponse;
+import com.crud.jobportal.module.job.dao.JobDao;
+import com.crud.jobportal.module.job.dto.JobDto;
+import com.crud.jobportal.module.job.vo.response.JobResponse;
 import org.apache.coyote.BadRequestException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -20,6 +27,12 @@ public class CandidateServiceImpl implements CandidateService {
 
     @Autowired
     private CandidateDao candidateDao;
+
+    @Autowired
+    private CandidateSkillDao candidateSkillDao;
+
+    @Autowired
+    private JobDao jobDao;
 
     @Override
     @Transactional
@@ -135,6 +148,82 @@ public class CandidateServiceImpl implements CandidateService {
          }
         return candidateResponseList;
     }
+
+    @Override
+    @Transactional
+    public CandidateSkillResponse createCandidateSkill(CreateCandidateSkillRequest createCandidateSkillRequest) {
+        CandidateSkillDto candidateSkillDto = CandidateSkillDto.builder()
+                .id(createCandidateSkillRequest.getId())
+                .skill(createCandidateSkillRequest.getSkill())
+                .yoe(createCandidateSkillRequest.getYoe())
+                .candidateId(createCandidateSkillRequest.getCandidateId())
+                .createdAt(createCandidateSkillRequest.getCreatedAt())
+                .createdBy(createCandidateSkillRequest.getCreatedBy())
+                .build();
+        CandidateSkillDto resposneCandidateSkillDto = candidateSkillDao.createCandidateSkill(candidateSkillDto);
+
+        CandidateSkillResponse candidateSkillResponse = CandidateSkillResponse.builder()
+                .id(resposneCandidateSkillDto.getId())
+                .skill(resposneCandidateSkillDto.getSkill())
+                .yoe(resposneCandidateSkillDto.getYoe())
+                .candidateId(resposneCandidateSkillDto.getCandidateId())
+                .createdAt(resposneCandidateSkillDto.getCreatedAt())
+                .createdBy(resposneCandidateSkillDto.getCreatedBy())
+                .build();
+
+        return candidateSkillResponse;
+    }
+
+    @Override
+    public CandidateSkillResponse getCandidateSkillById(Long id) {
+        CandidateSkillDto candidateSkillDto = candidateSkillDao.getCandidateSkillById(id);
+        CandidateSkillResponse candidateSkillResponse = CandidateSkillResponse.builder()
+                .id(candidateSkillDto.getId())
+                .skill(candidateSkillDto.getSkill())
+                .yoe(candidateSkillDto.getYoe())
+                .candidateId(candidateSkillDto.getCandidateId())
+                .createdBy(candidateSkillDto.getCreatedBy())
+                .createdAt(candidateSkillDto.getCreatedAt())
+                .build();
+        return candidateSkillResponse;
+    }
+
+    @Override
+    public List<JobResponse> getAllJobs() {
+        List<JobDto> jobDtoList = jobDao.getAllJobs();
+        List<JobResponse> jobResponseList = new ArrayList<>();
+
+        for (JobDto jobDto : jobDtoList) {
+            JobResponse jobResponse = JobResponse.builder()
+                    .id(jobDto.getId())
+                    .name(jobDto.getName())
+                    .industry(jobDto.getIndustry())
+                    .salary(jobDto.getSalary())
+                    .createdAt(jobDto.getCreatedAt())
+                    .build();
+            jobResponseList.add(jobResponse);
+        }
+        return jobResponseList;
+    }
+
+    @Override
+    @Transactional
+    public Long deleteCandidateSkill(Long id) {
+        CandidateSkillResponse candidateSkillResponse = this.getCandidateSkillById(id);
+        CandidateSkillDto candidateSkillDto = CandidateSkillDto.builder()
+                .id(candidateSkillResponse.getId())
+                .skill(candidateSkillResponse.getSkill())
+                .yoe(candidateSkillResponse.getYoe())
+                .candidateId(candidateSkillResponse.getCandidateId())
+                .createdAt(candidateSkillResponse.getCreatedAt())
+                .createdBy(candidateSkillResponse.getCreatedBy())
+                .build();
+        CandidateSkillDto deleteCandidateSkillDto = candidateSkillDao.deleteCandidateSkill(candidateSkillDto);
+        return deleteCandidateSkillDto.getId();
+    }
+
+
 }
+
 
 
